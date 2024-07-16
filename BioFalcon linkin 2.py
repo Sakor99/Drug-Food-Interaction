1739e2515e57c7a8d44e8f63c76c7e47ae52711a
Samer Sakor <158743589+Sakor99@users.noreply.github.com>
2024-07-16 14:05:49 +0200

    BioFalcon linkin 2.py

--- BioFalcon linkin 2.py
+++ BioFalcon linkin 2.py
@@ -26,7 +26,6 @@ def Biofalcon_call(text):
     else:
         return []
 
-# Read CSV file and extract desired columns
 csv_file_path = 'DFI__FORM2.csv'
 columns = ['Effect']
 
@@ -35,7 +34,6 @@ with open(csv_file_path, 'r', encoding='utf-8', errors='ignore') as file:
     reader = csv.DictReader(file)
     fieldnames = reader.fieldnames
 
-    # Create new fieldnames with updated column names
     new_fieldnames = []
     for column in fieldnames:
         new_fieldnames.append(column)
@@ -45,7 +43,6 @@ with open(csv_file_path, 'r', encoding='utf-8', errors='ignore') as file:
     for row in reader:
         texts = [row[column] for column in columns]
 
-        # Perform BioFalcon API call on each text
         results = []
         for text in texts:
             result = Biofalcon_call(text)
@@ -54,7 +51,6 @@ with open(csv_file_path, 'r', encoding='utf-8', errors='ignore') as file:
             else:
                 results.append(result)
 
-        # Add results and updated column names to the row
         new_row = {}
         for column, value in row.items():
             new_row[column] = value
@@ -64,7 +60,6 @@ with open(csv_file_path, 'r', encoding='utf-8', errors='ignore') as file:
 
         rows.append(new_row)
 
-# Write results to a new CSV file or overwrite the original file
 output_file_path = 'DFI__FORM3.csv'  # Replace with the desired output file path
 
 with open(output_file_path, 'w', encoding='utf-8', newline='') as file:
